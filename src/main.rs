ef53f1c5efc614e539e342c3f919d913a7431a3c
Nuutti Kotivuori <naked@iki.fi>
2021-12-01 17:41:36 +0000

    Basic scaffolding

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+fn main() -> Result<(), std::io::Error> {
+    day1()?;
+    Ok(())
+}
+
+fn day1() -> Result<(), std::io::Error> {
+    std::fs::read_to_string("inputs/input-01")?;
+    Ok(())
 }
