5b8dc2f1bbc83003fbe6369b6507e51cf6058968
Nuutti Kotivuori <naked@iki.fi>
2021-12-01 18:07:30 +0000

    Fix outputs

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,21 @@
 fn main() -> Result<(), std::io::Error> {
-    day1_puzzle1()?;
-    day1_puzzle2()?;
+    println!("day 1 puzzle 1: {}", day1_puzzle1()?);
+    println!("day 1 puzzle 2: {}", day1_puzzle2()?);
     Ok(())
 }
 
-fn day1_puzzle1() -> Result<(), std::io::Error> {
-    let count = std::fs::read_to_string("inputs/input-01")?
+fn day1_puzzle1() -> Result<usize, std::io::Error> {
+    Ok(std::fs::read_to_string("inputs/input-01")?
         .lines()
         .map(|x| x.parse::<i64>().unwrap())
         .collect::<Vec<i64>>()[..]
         .windows(2)
         .filter(|x| x[0] < x[1])
-        .count();
-    println!("{}", count);
-    Ok(())
+        .count())
 }
 
-fn day1_puzzle2() -> Result<(), std::io::Error> {
-    let count = std::fs::read_to_string("inputs/input-01")?
+fn day1_puzzle2() -> Result<usize, std::io::Error> {
+    Ok(std::fs::read_to_string("inputs/input-01")?
         .lines()
         .map(|x| x.parse::<i64>().unwrap())
         .collect::<Vec<i64>>()[..]
@@ -26,8 +24,5 @@ fn day1_puzzle2() -> Result<(), std::io::Error> {
         .collect::<Vec<i64>>()[..]
         .windows(2)
         .filter(|x| x[0] < x[1])
-        .count();
-    println!("{}", count);
-    Ok(())
+        .count())
 }
-
