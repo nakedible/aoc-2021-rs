ca54d0db953e3be1d114124f69d32b39b0c8e24d
Nuutti Kotivuori <naked@iki.fi>
2021-12-02 10:36:41 +0000

    Add placeholder for next one

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 fn main() -> Result<(), std::io::Error> {
     println!("day 1 puzzle 1: {}", day1_puzzle1()?);
     println!("day 1 puzzle 2: {}", day1_puzzle2()?);
+    println!("day 2 puzzle 1: {}", day2_puzzle1()?);
     Ok(())
 }
 
@@ -27,4 +28,9 @@ fn day1_puzzle2() -> Result<usize, std::io::Error> {
         .windows(2)
         .filter(|x| x[0] < x[1])
         .count())
+}
+
+#[inline(never)]
+fn day2_puzzle1() -> Result<usize, std::io::Error> {
+    Ok(0)
 }
