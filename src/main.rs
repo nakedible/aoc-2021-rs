5c3efefef5313b85c9be5c563624794082188ef9
Nuutti Kotivuori <naked@iki.fi>
2021-12-02 10:58:52 +0000

    Do the thing

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ fn main() -> Result<(), std::io::Error> {
     println!("day 1 puzzle 1: {}", day1_puzzle1()?);
     println!("day 1 puzzle 2: {}", day1_puzzle2()?);
     println!("day 2 puzzle 1: {}", day2_puzzle1()?);
+    println!("day 2 puzzle 2: {}", day2_puzzle2()?);
     Ok(())
 }
 
@@ -32,5 +33,40 @@ fn day1_puzzle2() -> Result<usize, std::io::Error> {
 
 #[inline(never)]
 fn day2_puzzle1() -> Result<usize, std::io::Error> {
-    Ok(0)
+    let (posh, depth) = std::fs::read_to_string("inputs/input-02")?
+        .lines()
+        .map(|x| {
+            let mut parts = x.split_whitespace();
+            (
+                parts.next().unwrap(),
+                parts.next().unwrap().parse::<i64>().unwrap(),
+            )
+        })
+        .fold((0, 0), |(posh, depth), (dir, amount)| match dir {
+            "forward" => (posh + amount, depth),
+            "down" => (posh, depth + amount),
+            "up" => (posh, depth - amount),
+            _ => panic!("invalid input"),
+        });
+    Ok((posh * depth) as usize)
+}
+
+#[inline(never)]
+fn day2_puzzle2() -> Result<usize, std::io::Error> {
+    let (posh, depth, _aim) = std::fs::read_to_string("inputs/input-02")?
+        .lines()
+        .map(|x| {
+            let mut parts = x.split_whitespace();
+            (
+                parts.next().unwrap(),
+                parts.next().unwrap().parse::<i64>().unwrap(),
+            )
+        })
+        .fold((0, 0, 0), |(posh, depth, aim), (dir, amount)| match dir {
+            "forward" => (posh + amount, depth + (aim * amount), aim),
+            "down" => (posh, depth, aim + amount),
+            "up" => (posh, depth, aim - amount),
+            _ => panic!("invalid input"),
+        });
+    Ok((posh * depth) as usize)
 }
